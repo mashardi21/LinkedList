42c46cfa8a7a531a54fa81ae1ca1b4c3feed1bc5
mashardi21 <mashardi@uat.edu>
2018-10-06 00:46:23 -0700

    Moved class function definitions into header file to fix linker errors

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -7,15 +7,71 @@
 template <class T>
 class LinkedList {
 public:
-	LinkedList();
-	~LinkedList();
-	T* Find(T dataToFind);
-	void insertNode(T newData);
-	void deleteNode(Node* delNode);
+	LinkedList() {
+		_headPtr = nullptr;
+		_tailPtr = nullptr;
+	}
+	~LinkedList() {
+
+	}
+	Node<T>* Find(T dataToFind) {
+		Node<T>* currentNode = *_headPtr;
+
+		while (currentNode->getNextNode() != nullptr) {
+			if (currentNode->getData() == dataToFind) {
+				return &currentNode;
+			}
+			else {
+				currentNode = currentNode->getNextNode();
+			}
+		}
+
+		return nullptr;
+	}
+	void insertNode(T newData) {
+		Node<T> newNode = Node<T>(newData);
+		Node<T>* nodePtr = &newNode;
+
+		if (_headPtr == nullptr) {
+			_headPtr = nodePtr;
+			_tailPtr = nodePtr;
+		}
+		else if (_headPtr != nullptr) {
+			newNode.setPrevNode(_tailPtr);
+			newNode.setNextNode(nullptr);
+			_tailPtr->setNextNode(&newNode);
+			_tailPtr = &newNode;
+		}
+
+		delete nodePtr;
+	}
+	void deleteNode(Node* delNode) {
+		if (_headPtr == nullptr || delNode->getPrevNode() == nullptr) {
+			return;
+		}
+
+		if (&delnode == &_headPtr) {
+			_headPtr = delNode->getNextNode();
+			_headPtr->setPrevNode(nullptr);
+		}
+		else if (&delNode == &_tailPtr) {
+			_tailPtr = delNode->getPrevNode();
+			_tailPtr->setNextNode(nullptr);
+		}
+		else {
+			Node<T>* prevNode = delNode->getPrevNode();
+			Node<T>* nextNode = delNode->getNextNode();
+
+			prevNode->setNextNode(nextNode);
+			nextNode->setPrevNode(prevNode);
+		}
+
+		delete delNode;
+	}
 
 private:
-	Node* headPtr;
-	Node* tailPtr;
+	Node* _headPtr;
+	Node* _tailPtr;
 
 };
 
--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -4,13 +4,29 @@
 template <class T>
 class Node {
 public:
-	Node(T data);
-	~Node();
-	T getData();
-	void setNextNode(T* nextNode);
-	void setPrevNode(T* prevNode);
-	T* getNextNode();
-	T* getPrevNode();
+	Node(T data) {
+		_data = data;
+		_nextNode = nullptr;
+		_prevNode = nullptr;
+	}
+	~Node() {
+
+	}
+	T getData() {
+		return _data;
+	}
+	void setNextNode(T* nextNode) {
+		_nextNode = nextNode;
+	}
+	void setPrevNode(T* prevNode) {
+		_prevNode = prevNode;
+	}
+	T* getNextNode() {
+		return _nextNode;
+	}
+	T* getPrevNode() {
+		return _prevNode;
+	}
 private:
 	T _data;
 	T* _nextNode;
